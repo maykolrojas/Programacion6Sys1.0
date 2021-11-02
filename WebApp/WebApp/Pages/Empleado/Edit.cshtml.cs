e81470d43abea9535d8e96f570fb2d56414964cc
maykolrojas <maykolrojas@hotmail.com>
2021-11-02 14:48:51 -0600

    commit semana 6

--- WebApp/WebApp/Pages/Empleado/Edit.cshtml.cs
+++ WebApp/WebApp/Pages/Empleado/Edit.cshtml.cs
@@ -4,13 +4,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Entity;
+using WBL;
 
 namespace WebApp.Pages.Empleado
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IEmpleadoService empleadoService;
+
+        public EditModel(IEmpleadoService empleadoService)
         {
+            this.empleadoService = empleadoService;
         }
+
+        [BindProperty]
+
+        public EmpleadoEntity Entity { get; set; } = new EmpleadoEntity();
+
+
+        [BindProperty (SupportsGet = true)]
+
+        public int? id { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        { 
+            try
+            {
+                if (id.HasValue)
+                {
+                    Entity = await empleadoService.GetById(entity: new() { IdEmpleado = id });
+
+                }
+
+                return Page();
+            }
+              catch (Exception ex)
+                 {
+                return Content(content: ex.Message);
+
+
+                 }
+            }
+
+
     }
-}
+
+    }
+           
+
+            
+
+            
+
+      
